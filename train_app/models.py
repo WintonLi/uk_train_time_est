3f1d729dc9f596f628a93f98b9dfc4c7957e8909
winton <yunxili@axiomamc.com>
2022-07-31 08:41:28 +0100

    remove db

--- train_app/models.py
+++ train_app/models.py
@@ -9,16 +9,24 @@ from datetime import datetime
 from db import Base, APPTZ
 
 
+class Interval(Base):
+    __tablename__ = "intervals"
+    id = Column(Integer, primary_key=True, index=True)
+    start_timestamp = Column(Integer)
+    stop_timestamp = Column(Integer)
+    items = relationship("Timetable", primaryjoin="Interval.id == Timetable.interval_id", cascade="all, delete")
+
+
 class Timetable(Base):
-    __tablename__ = "timetable"
+    __tablename__ = "timetables"
 
     id = Column(Integer, primary_key=True, index=True)
     station_code = Column(String(3), nullable=False)
-    window_id = Column(Integer)
     service = Column(Integer)
     train_uid = Column(String(20), nullable=False)
     aimed_departure_timestamp = Column(Integer)
     aimed_arrival_timestamp = Column(Integer)
+    interval_id = Column(Integer, ForeignKey('intervals.id'), nullable=False)
 
     def __repr__(self):
         return f'{self.station_code} {self.train_uid} {datetime.fromtimestamp(self.aimed_arrival_timestamp, tz=APPTZ)}'
